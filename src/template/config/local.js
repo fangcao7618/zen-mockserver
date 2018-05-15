ab43fb71a0c089d8c072685c0ddc115dfcac1d27
ssehacker <ssehacker@163.com>
2018-05-15 23:40:56 +0800

    fix template.

--- src/template/config/local.js
+++ src/template/config/local.js
@@ -2,18 +2,18 @@ module.exports = {
   port: 9000,
   proxy: [
     // 接口转发
-    {
-      path: '/posts',
-      target: 'https://jsonplaceholder.typicode.com',
-      pathRewrite: {
-        '^/posts' : '/photos',     // rewrite path
-      },
-    },
-    // 不以/api 开头的请求转发到 http://localhost:3000/
-    // 用以转发静态资源的请求
-    {
-      path: '^(?!/api)',
-      target: 'http://localhost:3000/',
-    },
+    // {
+    //   path: '/posts',
+    //   target: 'https://jsonplaceholder.typicode.com',
+    //   pathRewrite: {
+    //     '^/posts' : '/photos',     // rewrite path
+    //   },
+    // },
+    // // 不以/api 开头的请求转发到 http://localhost:3000/
+    // // 用以转发静态资源的请求
+    // {
+    //   path: '^(?!/api)',
+    //   target: 'http://localhost:3000/',
+    // },
   ]
 };
